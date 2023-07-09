0dd09570800cc539140101d723c8207bff464c10
minsoong2 <happyms817@gmail.com>
2023-07-09 15:57:04 +0900

    230709

--- mecab.py
+++ mecab.py
@@ -1,4 +1,6 @@
 import MeCab
+import tensorflow as tf
+
 mecab = MeCab.Tagger()
-out = mecab.parse("오늘은 맑은 날씨이다.")
+out = mecab.parse("본 고안은 주로 일회용 합성세제액을 집어넣어 밀봉하는 세제액포의 내부를 원호상으로 열중착하되 세제액이 배출되는 절단부 쪽으로 내벽을 협소하게 형성하여서 내부에 들어있는 세제액을 잘짜질 수 있도록 하는 합성세제 액포에 관한 것이다.")
 print(out)
